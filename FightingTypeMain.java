cc68f4af76e19acd5f03b4716e9e7ca4cbd8421f
HimaBindu <hima952014@gmail.com>
2026-03-04 22:40:21 +0000

    Bindu- Fighting and ICE Type   pokemon  flow controls java programs -04032026

---
+++ FightingTypeMain.java
@@ -0,0 +1,24 @@
+package pokedexBindu;
+
+import pokedexBindu.FightingType.Machamp;
+import pokedexBindu.FightingType.Lucario;
+import pokedexBindu.FightingType.IronValiant;
+
+public class FightingTypeMain {
+    public static void main(String[] args) {
+
+        Machamp m = new Machamp("Machamp", 40, 120, 4);
+        Lucario l = new Lucario("Lucario", 35, 95, 80);
+        IronValiant iv = new IronValiant("Iron Valiant", 50, 130, 90);
+
+        // Demonstrate behaviours//
+        m.dynamicPunch();
+        m.train();
+
+        l.auraSphere();
+        l.meditate();
+
+        iv.spiritBreak();
+        iv.stylishStrike();
+    }
+}
